6be6623a111ae2eff8db89be0dd513971ed37b09
ramoodles <98553164+ramoodles@users.noreply.github.com>
2022-02-01 13:41:02 -0500

    Update ball.js

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -1,1 +1,38 @@
+$(document).ready(function(){
+  
+  var mousePos = {};
+
+ function getRandomInt(min, max) {
+   return Math.round(Math.random() * (max - min + 1)) + min;
+ }
+  
+  $(window).mousemove(function(e) {
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
+  });
+  
+  $(window).mouseleave(function(e) {
+    mousePos.x = -1;
+    mousePos.y = -1;
+  });
+  
+  var draw = setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 15;
+      
+      var color = "background: rgb(0,0,0,.5);";
+      
+      var sizeInt = getRandomInt(5, 5);
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+      
+      var left = "left: " + mousePos.x + "px;";
+      
+      var top = "top: " + mousePos.y+ "px;"; 
+
+      var style = left+top+color+size;
+      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+    }
+  }, 1);
+});
 
